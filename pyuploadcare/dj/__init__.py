4ac9840edff90f43ed887d548ab71f3101d5994e
Valya Golev <me@valyagolev.net>
2011-11-18 12:07:49 +0400

    making south happy

--- pyuploadcare/dj/__init__.py
+++ pyuploadcare/dj/__init__.py
@@ -9,3 +9,12 @@ def UploadCare(**kwargs):
     return pyuploadcare.UploadCare(**kws)
 
 from pyuploadcare.dj.models import FileField
+
+
+# making South happy
+
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ["^pyuploadcare\.dj\."])
+except ImportError, e:
+    pass
