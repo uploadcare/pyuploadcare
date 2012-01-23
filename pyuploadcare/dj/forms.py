76611f60c417a43f750c5bfb0e4fb4d80fa19d06
Sławek Ehlert <slafs@op.pl>
2012-01-23 22:00:35 +0100

    adding support for (django.contrib.)staticfiles

--- pyuploadcare/dj/forms.py
+++ pyuploadcare/dj/forms.py
@@ -1,16 +1,37 @@
 from django.forms import Field, TextInput, Media
 from django.conf import settings
 from django.utils.safestring import mark_safe
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext as _, get_language
+from pyuploadcare.dj.conf import USE_HOSTED_ASSETS
 
-UPLOADCARE_JS = 'http://static.uploadcare.com/assets/uploaders/line-widget.en.js'
+UPLOADCARE_JS = 'http://static.uploadcare.com/assets/uploaders/line-widget.%(lang)s.js'
+UPLOADCARE_LOCAL_JS = 'uploadcare/assets/uploaders/line-widget.%(lang)s.js'
+
+AVAIL_ASSET_LANG = ('en', 'ru', 'pl')
+
+def get_asset_lang():
+    '''returns a dict with one key: "lang" '''
+    lang = get_language()
+    if lang.startswith('en'):
+        lang = lang[:2]
+
+    if not lang in AVAIL_ASSET_LANG:
+        lang = 'en'
+
+    BASE_ASSET_URL = UPLOADCARE_JS
+    if not USE_HOSTED_ASSETS:
+        BASE_ASSET_URL = UPLOADCARE_LOCAL_JS
+
+    return BASE_ASSET_URL % {'lang' : lang}
+
 
 class FileWidget(TextInput):
     input_type = 'hidden'
 
     class Media:
-        js = (UPLOADCARE_JS,)
+        js = (get_asset_lang(),)
     
+
     def __init__(self, attrs=None):
         from pyuploadcare.dj import UploadCare
 
