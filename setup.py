486191259ae0202e743307be6bfff1501cbf871b
Dmitry Mukhin <dmitry.s.mukhin@gmail.com>
2012-06-04 21:19:04 +0400

    add package data and classifiers to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 from setuptools import setup
 
-setup(name='pyuploadcare',
-      version='0.6',
-      description="UploadCare.com API library",
-      author='Valentin Golev',
-      author_email='v.golev@gmail.com',
-      url='https://github.com/uploadcare/pyuploadcare',
-      packages=['pyuploadcare', 'pyuploadcare.dj'],
-     )
+setup(
+    name='pyuploadcare',
+    version='0.6',
+    description="UploadCare.com API library",
+    author='Valentin Golev',
+    author_email='v.golev@gmail.com',
+    url='https://github.com/uploadcare/pyuploadcare',
+    packages=['pyuploadcare', 'pyuploadcare.dj'],
+    package_data={
+        'pyuploadcare.dj': [
+            'static/uploadcare/assets/uploaders/*.js',
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ]
+)
