babd369d6f0f320b74e0f8943eccfd6fae6fc7f3
marselester <marselester@ya.ru>
2013-04-01 21:12:22 +0700

    Add admin inlines example

--- test_project/gallery/models.py
+++ test_project/gallery/models.py
@@ -3,10 +3,20 @@ from django.db import models
 from pyuploadcare.dj import FileField, ImageField
 
 
-class Photo(models.Model):
+class Gallery(models.Model):
+
     title = models.CharField(max_length=255)
-    photo = FileField()
-    photo_2x3 = ImageField(manual_crop='2:3')
+
+    def __unicode__(self):
+        return self.title
+
+
+class Photo(models.Model):
+
+    gallery = models.ForeignKey(Gallery)
+    title = models.CharField(max_length=255)
+    arbitrary_file = FileField(blank=True, null=True)
+    photo_2x3 = ImageField(manual_crop='2:3', blank=True)
 
     def __unicode__(self):
         return self.title
