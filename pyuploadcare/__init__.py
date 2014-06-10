46677aa4baa842b5f74e8090f48fe6f8c6fcf12f
Dmitry Mukhin <dmitry.s.mukhin@gmail.com>
2014-06-10 10:30:16 +0400

    bump __version__

--- pyuploadcare/__init__.py
+++ pyuploadcare/__init__.py
@@ -15,7 +15,7 @@ Usage example::
 
 from __future__ import unicode_literals
 
-__version__ = '1.2.2'
+__version__ = '1.2.5'
 
 from .api_resources import File, FileList, FileGroup
 from .exceptions import (
