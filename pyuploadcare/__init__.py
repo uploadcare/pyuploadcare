6492d537f4a9d9e0adf1430b31b3302409e4fe9d
homm <homm86@gmail.com>
2014-07-31 17:34:19 +0400

    version

--- pyuploadcare/__init__.py
+++ pyuploadcare/__init__.py
@@ -15,7 +15,7 @@ Usage example::
 
 from __future__ import unicode_literals
 
-__version__ = '1.2.7'
+__version__ = '1.2.8'
 
 from .api_resources import File, FileList, FileGroup
 from .exceptions import (
