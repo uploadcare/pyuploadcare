47ff1759dea56a94c7e0466e9a1b1ff30ac9d3ed
D. Petrov <D.Petrov@petrosoftinc.com>
2015-06-04 21:02:12 +0300

    clearable field added

--- pyuploadcare/dj/models.py
+++ pyuploadcare/dj/models.py
@@ -90,7 +90,7 @@ class ImageField(FileField):
 
     """
 
-    def __init__(self, manual_crop=None, *args, **kwargs):
+    def __init__(self, manual_crop=None, clearable=None, *args, **kwargs):
         is_crop_valid = (
             isinstance(manual_crop, six.string_types) and
             all([pattern_of_crop.match(part) for part in manual_crop.split(',')])
@@ -99,10 +99,12 @@ class ImageField(FileField):
             raise ValidationError('Invalid manual crop value')
 
         self.manual_crop = manual_crop
+        self.clearable = clearable
         super(ImageField, self).__init__(*args, **kwargs)
 
     def formfield(self, **kwargs):
         kwargs['manual_crop'] = self.manual_crop
+        kwargs['clearable'] = self.clearable
         kwargs['form_class'] = forms.ImageField
 
         return models.Field.formfield(self, **kwargs)
