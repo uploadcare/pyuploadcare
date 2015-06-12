daada2d99aacd0c5cdbf295e450acd4a1f492967
Dmitry Mukhin <dm@uploadcare.com>
2015-06-12 18:44:49 +0200

    update test project's manage.py

--- test_project/manage.py
+++ test_project/manage.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
-from django.core.management import execute_manager
-import imp
-try:
-    imp.find_module('settings') # Assumed to be in the same directory.
-except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n" % __file__)
-    sys.exit(1)
 
-import settings
+import os
+
+from django.core import management
+
+if 'DJANGO_SETTINGS_MODULE' not in os.environ:
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
 
 if __name__ == "__main__":
-    execute_manager(settings)
+    management.execute_from_command_line()
