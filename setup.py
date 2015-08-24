8bd5c7490db2cba89b51cd024ec10ee3ef42e8ef
Dmitry Mukhin <dm@uploadcare.com>
2015-08-24 20:04:05 +0300

    bump version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ else:
 
 setup(
     name='pyuploadcare',
-    version='1.3.0',
+    version='1.3.1',
     description='Python library for Uploadcare.com',
     long_description=(long_description),
     author='Uploadcare LLC',
