17e5f21184e1461a02a2d4aadc98986f9ea4aca1
Henrik Heimbuerger <henrik@heimbuerger.de>
2015-09-10 09:41:55 +0200

    Add success case integration test for model form use case

--- tests/integration/test_models.py
+++ tests/integration/test_models.py
@@ -1,10 +1,12 @@
 # coding: utf-8
 from __future__ import unicode_literals
+from tests.integration.utils import upload_tmp_txt_file
 
 try:
     import unittest2 as unittest
 except ImportError:
     import unittest
+
 import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'test_project.settings'
 
@@ -16,17 +18,29 @@ from django.db import models
 from pyuploadcare.dj import models as uc_models
 
 
+class Employee(models.Model):
+    cv = uc_models.FileField()
+    class Meta:
+        app_label = 'tests.integration.test_models.Employee'
+
+
+class EmployeeForm(forms.ModelForm):
+    class Meta:
+        model = Employee
+        fields = ['cv']
+
+
 class ModelFormIntegrationTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.file_ = upload_tmp_txt_file(content='hello')
+
+    def test_no_validation_error_if_uuid_is_valid(self):
+        form = EmployeeForm(data={'cv': ModelFormIntegrationTest.file_.uuid})
+        form.full_clean()
+        self.assertEquals(form.errors, {})
 
     def test_validation_error_if_uuid_is_invalid(self):
-        class Employee(models.Model):
-            cv = uc_models.FileField()
-            class Meta:
-                app_label = 'tests.integration.test_models.test_validation_error_if_uuid_is_invalid.Employee'
-        class EmployeeForm(forms.ModelForm):
-            class Meta:
-                model = Employee
-                fields = ['cv']
         form = EmployeeForm(data={'cv': '99999999-9999-9999-9999-999999999999'})
         form.full_clean()
         self.assertIsNotNone(form.errors)
