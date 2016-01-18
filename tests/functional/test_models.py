e7cb1cabfa414aaf1ad37da83f92d1fc874bd47e
zero13cool <zero13cool@yandex.ru>
2016-01-18 16:31:27 +0300

    django1.9 compatibility for tests

--- tests/functional/test_models.py
+++ tests/functional/test_models.py
@@ -27,29 +27,6 @@ from pyuploadcare.api_resources import File, FileGroup
 from pyuploadcare.exceptions import InvalidRequestError
 
 
-# Just for calling `value.info` inside `Field.clean` method.
-faked_size_validator = lambda value: value.info()['size']
-
-
-class Employee(models.Model):
-    cv = FileField()
-    cv_with_validators = FileField(validators=[faked_size_validator])
-    cv_blank = FileField(blank=True)
-    cv_null = FileField(null=True)
-
-    class Meta:
-        app_label = 'tests'
-
-
-class Book(models.Model):
-    pages = FileGroupField()
-    pages_blank = FileGroupField(blank=True)
-    pages_null = FileGroupField(null=True)
-
-    class Meta:
-        app_label = 'tests'
-
-
 class CropToolRegexTest(unittest.TestCase):
 
     def test_crop_tool_is_none(self):
@@ -89,28 +66,56 @@ class CropToolRegexTest(unittest.TestCase):
 class FileFieldTest(unittest.TestCase):
 
     def test_empty_str_is_allowed(self):
+        class Employee(models.Model):
+            class Meta:
+                app_label = 'tests'
+            cv = FileField(blank=True)
         emp = Employee()
-        self.assertEqual(emp.cv_blank, '')
+        self.assertEqual(emp.cv, '')
 
     def test_null_is_allowed(self):
+        class Employee(models.Model):
+            class Meta:
+                app_label = 'tests'
+            cv = FileField(null=True)
         emp = Employee(cv=None)
-        self.assertEqual(emp.cv_null, None)
+        self.assertEqual(emp.cv, None)
 
     def test_validation_error_if_value_is_int(self):
+        class Employee(models.Model):
+            class Meta:
+                app_label = 'tests'
+            cv = FileField()
         with self.assertRaises(ValidationError):
             Employee(cv=123)
 
     def test_validation_error_if_uuid_is_invalid_str(self):
+        class Employee(models.Model):
+            class Meta:
+                app_label = 'tests'
+            cv = FileField()
         with self.assertRaises(ValidationError):
             Employee(cv='123')
 
     def test_file_instance_if_uuid_is_valid(self):
+        class Employee(models.Model):
+            class Meta:
+                app_label = 'tests'
+            cv = FileField()
         emp = Employee(cv='3addab78-6368-4c55-ac08-22412b6a2a4c')
         self.assertIsInstance(emp.cv, File)
 
     @patch('pyuploadcare.api_resources.rest_request', autospec=True)
     def test_not_found_file_uuid(self, request):
         request.side_effect = InvalidRequestError(404)
+
+        # Just for calling `value.info` inside `Field.clean` method.
+        faked_size_validator = lambda value: value.info()['size']
+
+        class Employee(models.Model):
+            class Meta:
+                app_label = 'tests'
+            cv = FileField(validators=[faked_size_validator])
 
         emp = Employee(cv='3addab78-6368-4c55-ac08-22412b6a2a4c')
 
@@ -121,21 +126,41 @@ class FileFieldTest(unittest.TestCase):
 class FileGroupFieldTest(unittest.TestCase):
 
     def test_empty_str_is_allowed(self):
+        class Book(models.Model):
+            class Meta:
+                app_label = 'tests'
+            pages = FileGroupField(blank=True)
         book = Book()
         self.assertEqual(book.pages, '')
 
     def test_null_is_allowed(self):
+        class Book(models.Model):
+            class Meta:
+                app_label = 'tests'
+            pages = FileGroupField(null=True)
         book = Book(pages=None)
         self.assertEqual(book.pages, None)
 
     def test_validation_error_if_value_is_int(self):
+        class Book(models.Model):
+            class Meta:
+                app_label = 'tests'
+            pages = FileGroupField()
         with self.assertRaises(ValidationError):
             Book(pages=123)
 
     def test_validation_error_if_group_id_is_invalid_str(self):
+        class Book(models.Model):
+            class Meta:
+                app_label = 'tests'
+            pages = FileGroupField()
         with self.assertRaises(ValidationError):
             Book(pages='123')
 
     def test_file_group_instance_if_group_id_is_valid(self):
+        class Book(models.Model):
+            class Meta:
+                app_label = 'tests'
+            pages = FileGroupField()
         book = Book(pages='0513dda0-582f-447d-846f-096e5df9e2bb~2')
         self.assertIsInstance(book.pages, FileGroup)
