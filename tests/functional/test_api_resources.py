1cf883e1bfb1ef7f494e678508829109b49fad5e
andrewshkovskii <andrewshkovskii@gmail.com>
2017-03-09 19:12:43 +0700

    Remove linebreak

--- tests/functional/test_api_resources.py
+++ tests/functional/test_api_resources.py
@@ -253,7 +253,6 @@ class FileCopyTest(unittest.TestCase):
             "pattern": "${uuid}"})
 
 
-
 class FileGroupAsContainerTypeTest(unittest.TestCase):
 
     @patch('requests.sessions.Session.request', autospec=True)
