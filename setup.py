f806d2d23c1240eb21a1cff5d24451bb3bc78a45
Dmitry Mukhin <dm@uploadcare.com>
2020-04-16 18:34:41 +0200

    Give up on python 3.4

--- setup.py
+++ setup.py
@@ -45,7 +45,6 @@ setup(
         'Intended Audience :: Developers',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
@@ -54,6 +53,8 @@ setup(
         'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
         'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
         'License :: OSI Approved :: MIT License',
     ],
 )
