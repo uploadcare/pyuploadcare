0d9542011b78dca40f0c4c18a932a06d5305f6ef
Alexander Perlamutrov <wreckah@ya.ru>
2021-05-13 18:23:33 +0300

    Make the example code compatible with Python3

--- examples/delete_old_files.py
+++ examples/delete_old_files.py
@@ -20,14 +20,14 @@ dt_cutoff = datetime.now(pytz.utc) - timedelta(days=MAX_LIFETIME)
 
 if __name__ == '__main__':
 
-    print 'Selecting files to be deleted...'
+    print('Selecting files to be deleted...')
     uuid_list = [f.uuid for f in FileList(starting_point=dt_cutoff,
                                           ordering='-datetime_uploaded',
                                           stored=True,
                                           request_limit=500)]
-    print 'Batch delete of selected files'
+    print('Batch delete of selected files')
     ts1 = time.time()
     fs = FilesStorage(uuid_list)
     fs.delete()
     ts2 = time.time()
-    print 'Operation completed in %f seconds' % (ts2 - ts1)
+    print('Operation completed in %f seconds' % (ts2 - ts1))
