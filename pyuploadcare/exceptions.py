1d4a217220e82f9e694d56518c9636e86329316f
Denis Ivlev <di-erz@evrone.com>
2021-09-02 17:03:30 +0300

    Add http error handler

---
+++ pyuploadcare/exceptions.py
@@ -0,0 +1,18 @@
+from httpx import Request, Response
+
+
+class UploadCareError(Exception):
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+
+
+class UploadCareHTTPStatusError(UploadCareError):
+    def __init__(self, request: "Request", response: "Response") -> None:
+        self.request = request
+        self.response = response
+        if response.status_code == 429:
+            wait_time = response.headers.get("Retry-After")
+            super().__init__(
+                f"Number of seconds to wait before the next request = '{wait_time}'."
+            )
+        super().__init__(response.json().get("detail", None))
