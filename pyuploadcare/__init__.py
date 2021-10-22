5e49ee1c637360750143f37b3943fa5d710c0ed1
Artem Vasilyev <artem.v.vasilyev@gmail.com>
2021-10-22 16:31:30 +0300

    Feature/project client (#171)
    * getting project info by Uploadcare client
    
    * described getting project information

--- pyuploadcare/__init__.py
+++ pyuploadcare/__init__.py
@@ -5,5 +5,5 @@ from pyuploadcare.resources.file import File  # noqa: F401
 from pyuploadcare.resources.file_group import FileGroup  # noqa: F401
 from pyuploadcare.resources.file_list import FileList  # noqa: F401
 from pyuploadcare.resources.group_list import GroupList  # noqa: F401
-from pyuploadcare.api.entities import Webhook  # noqa: F401
+from pyuploadcare.api.entities import Webhook, ProjectInfo  # noqa: F401
 from pyuploadcare.client import Uploadcare  # noqa: F401
