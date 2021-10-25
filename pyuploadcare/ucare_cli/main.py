43285144354932c2e464d39160d684f4e97d3716
a.vasilyev <a.vasilyev@evrone.com>
2021-10-25 17:58:25 +0300

    cli get_project command

--- pyuploadcare/ucare_cli/main.py
+++ pyuploadcare/ucare_cli/main.py
@@ -14,6 +14,7 @@ from pyuploadcare.ucare_cli.commands import (
     create_group,
     delete_files,
     get_file,
+    get_project,
     list_files,
     list_groups,
     store_files,
@@ -46,6 +47,7 @@ def ucare_argparser():
     sync.register_arguments(subparsers)
     create_group.register_arguments(subparsers)
     convert_video.register_arguments(subparsers)
+    get_project.register_arguments(subparsers)
 
     # common arguments
     parser.add_argument(
