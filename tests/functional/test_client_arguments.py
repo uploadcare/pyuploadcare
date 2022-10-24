ff9b70ef386df2ae610b05886c3bb9a0f76b0c25
redbread <i.v.markov@ya.ru>
2022-10-24 03:38:38 +0300

    UCM-14: fix formatting

--- tests/functional/test_client_arguments.py
+++ tests/functional/test_client_arguments.py
@@ -23,7 +23,10 @@ def test_deprecated_argument_for_PY37_if_allow_is_set(test_client, caplog):
     assert not pyuploadcare.api.client.PY36
     assert pyuploadcare.api.client.PY37_AND_HIGHER
 
-    with pytest.raises(TypeError, match="got an unexpected keyword argument 'follow_redirects'"):
+    with pytest.raises(
+        TypeError,
+        match="got an unexpected keyword argument 'follow_redirects'",
+    ):
         test_client.request(
             method="any",
             url="yandex.ru",
