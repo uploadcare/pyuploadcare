8376f429e98f8cd9c850756b9f728e39fefacda3
Dmitry Mukhin <dm@uploadcare.com>
2022-11-11 14:01:52 +0100

    Fix Akamai signature generation

--- tests/functional/test_secure_url.py
+++ tests/functional/test_secure_url.py
@@ -4,9 +4,11 @@ from pyuploadcare import Uploadcare
 from pyuploadcare.secure_url import AkamaiSecureUrlBuilder
 
 
+known_secret = "73636b61519adede42191efe1e73f02a67c7b692e3765f90c250c230be095211"
+
 @pytest.mark.freeze_time("2021-10-12")
 def test_generate_secure_url():
-    secure_url_bulder = AkamaiSecureUrlBuilder("cdn.yourdomain.com", "secret")
+    secure_url_bulder = AkamaiSecureUrlBuilder("cdn.yourdomain.com", known_secret)
     secure_url = secure_url_bulder.build(
         "52da3bfc-7cd8-4861-8b05-126fef7a6994"
     )
@@ -14,13 +16,13 @@ def test_generate_secure_url():
         "https://cdn.yourdomain.com/52da3bfc-7cd8-4861-8b05-126fef7a6994/?token="
         "exp=1633997100~"
         "acl=/52da3bfc-7cd8-4861-8b05-126fef7a6994/~"
-        "hmac=a33cfc66c3e3592e712cdd1f82bd79d51df93b06"
+        "hmac=81852547d9dbd9eefd24bee2cada6eab02244b9013533bc8511511923098df72"
     )
 
 
 @pytest.mark.freeze_time("2021-10-12")
 def test_generate_secure_url_with_transformation():
-    secure_url_bulder = AkamaiSecureUrlBuilder("cdn.yourdomain.com", "secret")
+    secure_url_bulder = AkamaiSecureUrlBuilder("cdn.yourdomain.com", known_secret)
     secure_url = secure_url_bulder.build(
         "52da3bfc-7cd8-4861-8b05-126fef7a6994/-/resize/640x/other/transformations/"
     )
@@ -29,13 +31,13 @@ def test_generate_secure_url_with_transformation():
         "-/resize/640x/other/transformations/?token="
         "exp=1633997100~"
         "acl=/52da3bfc-7cd8-4861-8b05-126fef7a6994/-/resize/640x/other/transformations/~"
-        "hmac=24d11299339bdf9dcba26c7a5603c7ca63503fda"
+        "hmac=a3ae2b3e3adfcb5d41ca753598e19d17264ac87d2b0b828ccdac5136e63f2f1a"
     )
 
 
 @pytest.mark.freeze_time("2021-10-12")
 def test_client_generate_secure_url():
-    secure_url_bulder = AkamaiSecureUrlBuilder("cdn.yourdomain.com", "secret")
+    secure_url_bulder = AkamaiSecureUrlBuilder("cdn.yourdomain.com", known_secret)
 
     uploadcare = Uploadcare(
         public_key="public",
@@ -49,5 +51,5 @@ def test_client_generate_secure_url():
         "https://cdn.yourdomain.com/52da3bfc-7cd8-4861-8b05-126fef7a6994/?token="
         "exp=1633997100~"
         "acl=/52da3bfc-7cd8-4861-8b05-126fef7a6994/~"
-        "hmac=a33cfc66c3e3592e712cdd1f82bd79d51df93b06"
+        "hmac=81852547d9dbd9eefd24bee2cada6eab02244b9013533bc8511511923098df72"
     )
