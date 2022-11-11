25728b102f4713d5eac4144b59ee8af700ef9698
Dmitry Mukhin <dm@uploadcare.com>
2022-11-11 14:31:44 +0100

    Blackify

--- tests/functional/test_secure_url.py
+++ tests/functional/test_secure_url.py
@@ -4,11 +4,16 @@ from pyuploadcare import Uploadcare
 from pyuploadcare.secure_url import AkamaiSecureUrlBuilder
 
 
-known_secret = "73636b61519adede42191efe1e73f02a67c7b692e3765f90c250c230be095211"
+known_secret = (
+    "73636b61519adede42191efe1e73f02a67c7b692e3765f90c250c230be095211"
+)
+
 
 @pytest.mark.freeze_time("2021-10-12")
 def test_generate_secure_url():
-    secure_url_bulder = AkamaiSecureUrlBuilder("cdn.yourdomain.com", known_secret)
+    secure_url_bulder = AkamaiSecureUrlBuilder(
+        "cdn.yourdomain.com", known_secret
+    )
     secure_url = secure_url_bulder.build(
         "52da3bfc-7cd8-4861-8b05-126fef7a6994"
     )
@@ -22,7 +27,9 @@ def test_generate_secure_url():
 
 @pytest.mark.freeze_time("2021-10-12")
 def test_generate_secure_url_with_transformation():
-    secure_url_bulder = AkamaiSecureUrlBuilder("cdn.yourdomain.com", known_secret)
+    secure_url_bulder = AkamaiSecureUrlBuilder(
+        "cdn.yourdomain.com", known_secret
+    )
     secure_url = secure_url_bulder.build(
         "52da3bfc-7cd8-4861-8b05-126fef7a6994/-/resize/640x/other/transformations/"
     )
@@ -37,7 +44,9 @@ def test_generate_secure_url_with_transformation():
 
 @pytest.mark.freeze_time("2021-10-12")
 def test_client_generate_secure_url():
-    secure_url_bulder = AkamaiSecureUrlBuilder("cdn.yourdomain.com", known_secret)
+    secure_url_bulder = AkamaiSecureUrlBuilder(
+        "cdn.yourdomain.com", known_secret
+    )
 
     uploadcare = Uploadcare(
         public_key="public",
