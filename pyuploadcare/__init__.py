25ce62d20ff5116d1963d5539bde63edb13dfb69
Dmitry Mukhin <dm@uploadcare.com>
2022-11-25 16:36:45 +0100

    Update version

--- pyuploadcare/__init__.py
+++ pyuploadcare/__init__.py
@@ -1,5 +1,5 @@
 # isort: skip_file
-__version__ = "3.1.1"
+__version__ = "3.2.0"
 
 from pyuploadcare.resources.file import File  # noqa: F401
 from pyuploadcare.resources.file_group import FileGroup  # noqa: F401
