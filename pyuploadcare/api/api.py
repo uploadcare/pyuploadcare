uploadcare/pyuploadcare

commit e8fdd98d197bdf045907fdc33645490b116796cd

#239 FileAlreadyUploaded -> DuplicateFileError

pyuploadcare/api/api.py
@@ -26,7 +26,7 @@ from pyuploadcare.api.base import (
 )
 from pyuploadcare.exceptions import (
     APIError,
-    FileAlreadyUploaded,
+    DuplicateFileError,
     InvalidRequestError,
     WebhookIsNotUnique,
 )
@@ -393,7 +393,7 @@ class UploadAPI(API):
         if "token" not in response:
             if check_duplicates and response["type"] == "file_info":
                 file_id = response["file_id"]
-                raise FileAlreadyUploaded(
+                raise DuplicateFileError(
                     f"The file is a duplicate of a previously uploaded file ({file_id})",
                     file_id=file_id,
                 )

pyuploadcare/exceptions.py
@@ -59,7 +59,7 @@ class UploadError(UploadcareException):
     """
 
 
-class FileAlreadyUploaded(UploadError):
+class DuplicateFileError(UploadError):
     """Raised within UploadAPI.upload_from_url if check_duplicates is True and file was already been uploaded before)"""
 
     file_id: str

pyuploadcare/client.py
@@ -28,7 +28,7 @@ from pyuploadcare.api import (
 from pyuploadcare.api.auth import UploadcareAuth
 from pyuploadcare.api.client import Client
 from pyuploadcare.api.entities import ProjectInfo, Webhook
-from pyuploadcare.exceptions import FileAlreadyUploaded, InvalidParamError
+from pyuploadcare.exceptions import DuplicateFileError, InvalidParamError
 from pyuploadcare.helpers import (
     extracts_uuids,
     get_file_size,
@@ -474,7 +474,7 @@ class Uploadcare:
                 or source URL. Defaults to None.
             - check_duplicates (Optional[bool]): Enables duplicate uploads
                 prevention. If a file with the same source URL has been
-                previously uploaded this method will raise FileAlreadyUploaded.
+                previously uploaded this method will raise DuplicateFileError.
             - save_duplicates (Optional[bool]): Indicates if the URL should be
                 stored by Uploadcare future check_duplicates usages.
 
@@ -565,7 +565,7 @@ class Uploadcare:
                 until_ready=until_ready,
                 callback=callback,
             )
-        except FileAlreadyUploaded as e:
+        except DuplicateFileError as e:
             return self.file(e.file_id)
 
     def _extract_uuids(

tests/integration/test_api_resources.py
@@ -11,7 +11,7 @@ import pytest
 
 from pyuploadcare import File, FileGroup, conf
 from pyuploadcare.exceptions import (
-    FileAlreadyUploaded,
+    DuplicateFileError,
     InvalidParamError,
     InvalidRequestError,
 )
@@ -139,9 +139,9 @@ def test_successful_upload_from_url_check_duplicates(uploadcare):
         url, check_duplicates=True, store=False, interval=1
     )
     assert isinstance(first_file, File)
-    with pytest.raises(FileAlreadyUploaded) as e:
+    with pytest.raises(DuplicateFileError) as e:
         uploadcare.upload_from_url(url, check_duplicates=True, store=False)
-        assert isinstance(e, FileAlreadyUploaded)
+        assert isinstance(e, DuplicateFileError)
         assert first_file.uuid == e.file_id
 
 

commit 7270310415fcc182275a3fe8a4aa8afdc9124f8b

Fix flake8 warnings caused by #240

pyuploadcare/api/api.py
@@ -346,7 +346,7 @@ class UploadAPI(API):
         document = self._client.post(url, data=data)
         return document.json()
 
-    def upload_from_url(
+    def upload_from_url(  # noqa: max-complexity: 8
         self,
         source_url,
         store="auto",

pyuploadcare/exceptions.py
@@ -60,7 +60,10 @@ class UploadError(UploadcareException):
 
 
 class DuplicateFileError(UploadError):
-    """Raised within UploadAPI.upload_from_url if check_duplicates is True and file was already been uploaded before)"""
+    """
+    Raised within UploadAPI.upload_from_url if check_duplicates is True
+    and file was already been uploaded before)
+    """
 
     file_id: str
 